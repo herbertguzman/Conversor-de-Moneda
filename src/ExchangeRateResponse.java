d870528b6bcc92379a8c75000375247f4d57bf49
Herbert <91335146+herbertguzman@users.noreply.github.com>
2024-07-08 14:45:56 -0600

    Add files via upload
    Ending project, reupload due git bash error

---
+++ src/ExchangeRateResponse.java
@@ -0,0 +1,11 @@
+public class ExchangeRateResponse {
+    private double conversion_rate;
+
+    public double getConversionRate() {
+        return conversion_rate;
+    }
+
+    public void setConversionRate(double conversion_rate) {
+        this.conversion_rate = conversion_rate;
+    }
+}
